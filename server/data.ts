0e035c77f0740338c04b3df776c6e86a76d59d82
Michal <hello@michalk.nl>
2024-06-05 11:59:18 +0200

    added data files, for the backend.

---
+++ server/data.ts
@@ -0,0 +1,73 @@
+export const news = [
+  {
+    id: 0,
+    title: "Breaking News: Earthquake Strikes City",
+    content:
+      "A powerful earthquake measuring 7.5 on the Richter scale struck the city today, causing widespread damage and panic among residents.",
+    picture:
+      "https://www.shutterstock.com/shutterstock/photos/1928997539/display_1500/stock-vector-breaking-news-template-with-d-red-and-blue-badge-breaking-news-text-on-dark-blue-with-earth-and-1928997539.jpg",
+  },
+  {
+    id: 1,
+    title: "New Study Reveals Benefits of Exercise",
+    content:
+      "A recent study conducted by scientists at a renowned university has found that regular exercise can significantly improve overall health and reduce the risk of chronic diseases.",
+    picture:
+      "https://i.abcnewsfe.com/a/3cb8ba6c-ccdb-48de-99cc-e684e5358708/abcnl__NEW_streamingnow_1664457649883_hpMain_16x9.jpg?w=608",
+  },
+  {
+    id: 2,
+    title: "Film Festival Announces Lineup for This Year",
+    content:
+      "The highly anticipated film festival has announced its lineup for this year, featuring a diverse selection of movies from around the world. Movie enthusiasts are eagerly awaiting the event.",
+    picture:
+      "https://www.film-tv-video.de/wp-content/uploads/2019/02/B_0219_Kurznews.jpg",
+  },
+  {
+    id: 3,
+    title: "Breaking News: Political Scandal Uncovered",
+    content:
+      "A major political scandal has been uncovered, involving high-ranking officials and allegations of corruption. The news has sent shockwaves through the country's political landscape.",
+    picture:
+      "https://resize.indiatvnews.com/en/centered/newbucket/1200_675/2023/12/breaking-news-template-5-1703898487-1703988726.jpg",
+  },
+  {
+    id: 4,
+    title: "New Technology Breakthrough Revolutionizes Industry",
+    content:
+      "Scientists have made a groundbreaking discovery that promises to revolutionize the industry. The new technology has the potential to transform various sectors and improve efficiency.",
+    picture: "https://via.placeholder.com/150",
+  },
+];
+
+export const products = [
+  {
+    id: 0,
+    name: "Product 1",
+    picture:
+      "https://www.lego.com/cdn/cs/set/assets/blt0f703dfbce999d88/Video-games-Juno-Carousel-card.jpg?fit=crop&format=jpg&quality=80&width=635&height=440&dpr=1",
+  },
+  {
+    id: 1,
+    name: "Product 2",
+    picture: "https://static-prod-web.miniclip.com/assets/news/17_PR_SYBO.jpg",
+  },
+  {
+    id: 2,
+    name: "Product 3",
+    picture:
+      "https://images.twinkl.co.uk/tw1n/image/private/t_630/u/builder/board-games-logo-colour-cmyk-1687979724.png",
+  },
+  {
+    id: 3,
+    name: "Product 4",
+    picture:
+      "https://i.guim.co.uk/img/media/c6f7b43fa821d06fe1ab4311e558686529931492/168_84_1060_636/master/1060.jpg?width=465&dpr=1&s=none",
+  },
+  {
+    id: 4,
+    name: "Product 5",
+    picture:
+      "https://cn.i.cdn.ti-platform.com/content/92/roblox-game-on/link/za/cn_emea_roblox_gameon_banners_1920x1080_01.32c094ee.jpg",
+  },
+];
